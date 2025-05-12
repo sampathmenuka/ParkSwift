1ef349afe5211f05614ca2e19793adbf8f2c4a6d
Hasmoonn <mohamedhasmoon175@gmail.com>
2025-05-12 14:13:19 +0530

    Navbar.jsx added with login and logout

--- frontend/src/pages/Index.jsx
+++ frontend/src/pages/Index.jsx
@@ -1,8 +1,31 @@
 import React from 'react'
+import Navbar from '../components/Navbar'
+import FeatureSection from '../components/FeatureSection'
+import HowItWorks from '../components/HowItWorks'
+import Testimonials from '../components/Testimonials'
+import CallToAction from '../components/CallToAction'
+import Footer from '../components/Footer'
 
 const Index = () => {
   return (
-    <div>Index</div>
+    <div className='flex flex-col min-h-screen'>
+
+      <Navbar />
+
+      {/* Hero section  */}
+
+      <FeatureSection />
+
+      <HowItWorks />
+
+      <HowItWorks />
+
+      <Testimonials />
+
+      <CallToAction />
+
+      <Footer />
+    </div>
   )
 }
 
--- frontend/src/pages/SearchParking.jsx
+++ frontend/src/pages/SearchParking.jsx
@@ -1,8 +1,11 @@
 import React from 'react'
+import Navbar from '../components/Navbar'
 
 const SearchParking = () => {
   return (
-    <div>SearchParking</div>
+    <div>
+      <Navbar />
+    </div>
   )
 }
 
