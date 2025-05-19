872081f48d888a6520580d950ededeb84a18b343
Hasmoonn <mohamedhasmoon175@gmail.com>
2025-05-19 11:26:04 +0530

    user dashboard created successfully

---
+++ frontend/src/components/dashboard/Notification.jsx
@@ -0,0 +1,161 @@
+import React, { useContext, useState, useEffect } from 'react'
+import { AuthContext } from '../../contexts/AuthContext'
+import { toast } from 'react-toastify'
+import axios from 'axios'
+
+
+const Notification = () => {
+
+  const { backendUrl } = useContext(AuthContext)
+
+  const [notifications, setNotifications] = useState([])
+  const [unReadCount, setUnReadCount] = useState(0)
+
+  const fetchNotifications = async () => {
+
+    try {
+      
+      const {data} = await axios.get(backendUrl + '/api/notifications/')
+
+      if (data.success) {
+        setNotifications(data.notifications)
+        const unread = data.notifications.filter((e) => !e.isRead).length;
+
+        setUnReadCount(unread)
+      } else {
+        toast.error(data.message)
+      }
+    } catch (error) {
+        toast.error(error.message)
+    }
+  }
+
+  const handleMarkAsRead = async (id, isRead) =>{
+
+    try {
+
+      const {data} = await axios.patch(backendUrl + `/api/notifications/${id}`, {read: !isRead} );
+
+      if (data.success) {
+        setNotifications((prev) => prev.map((n) => (n._id === id ? { ...n, isRead: !isRead } : n)));
+        toast.success(data.message)
+      } else{
+        toast.error(data.message)
+      }
+
+      fetchNotifications();
+      
+    } catch (error) {
+        toast.error(error.message)
+    }
+  }
+
+
+  const handleMarkAllAsRead = async () => {
+
+    try{
+      const {data} = await axios.patch(backendUrl + '/api/notifications');
+
+      if (data.success) {
+        const updated = notifications.map((n) => ({...n, isRead: true}));
+
+        setNotifications(updated)
+        setUnReadCount(0);
+        toast.success(data.message);
+
+        fetchNotifications();
+      } else{
+          toast.error(data.message)
+      }
+    } catch(error){
+        toast.error(error.message)
+    }
+  }
+
+  
+  useEffect(() => {
+    fetchNotifications();
+  }, [backendUrl])
+
+  return (
+    <div>
+      <h2 className='text-2xl font-semibold mb-4 text-gray-700'>
+        Notifications
+      </h2>
+
+      {
+        notifications.length < 0 
+        ? (
+          <div className='text-center py-12'>
+            <p className='text-gray-600 mb-3 font-light text-lg'>
+              You have no Notifications.
+            </p>
+          </div>
+        ) : (
+          <div>
+            <div className='p-4 border rounded shadow mb-4'>
+              <div className='w-full flex items-center justify-between mb-6'>
+                <div>
+                  <h3 className='text-md md:text-2xl font-semibold text-indigo-500 tracking-wide'>
+                    Recent Notifications
+                    {
+                      unReadCount > 0 && (
+                        <span className='bg-red-500 ml-1 text-white text-sm md:text-base font-semibold rounded-full px-2 py-0.5'>            
+                          {unReadCount}
+                        </span>
+                      )
+                    }
+                  </h3>     
+                </div>
+
+                {
+                  unReadCount > 0 && (
+                    <button onClick={handleMarkAllAsRead} className='py-2 px-3 rounded bg-indigo-200 text-gray-800 hover:bg-indigo-400 text-xs md:text-sm transition-all duration-300'>
+                      Mark all as read
+                    </button>
+                  )
+                }
+                
+              </div>
+
+              {
+                notifications.map((notification) => (
+                  <div key={notification._id} className='p-4 rounded'>
+                    <div className='w-full'>
+                      <div className='w-full flex items-center justify-between mb-6'>
+                        <h3 className='text-md md:text-xl font-semibold text-gray-700'>
+                          {(notification.type).charAt(0).toUpperCase() + (notification.type).slice(1).toLowerCase()}
+                        </h3>
+                        <p className='text-xs sm:text-sm font-base text-gray-400'>
+                          {new Date(notification.createdAt).toDateString() }
+                        </p>
+                      </div> 
+
+                      <p className='text-gray-500 mb-4'>
+                        {notification.message}
+                      </p> 
+
+                      {
+                        !notification.isRead && (
+                          <div className='flex items-center justify-end mb-2'>
+                            <button onClick={() => handleMarkAsRead(notification._id, notification.isRead)} className='py-2 px-3 rounded bg-indigo-200 text-gray-800 hover:bg-indigo-400 text-xs md:text-sm transition-all duration-300' >
+                              Mark as read
+                            </button>
+                          </div>
+                        )
+                      }
+                      
+                      <hr />
+                    </div>
+                  </div>
+                ))
+              }
+            </div>           
+          </div>
+        )
+      }
+    </div>
+  )
+}
+
+export default Notification
--- frontend/src/pages/UserDashboard.jsx
+++ frontend/src/pages/UserDashboard.jsx
@@ -1,8 +1,53 @@
-import React from 'react'
+import React, { useContext, useState } from 'react'
+import { NavLink, Outlet, useNavigate } from 'react-router-dom'
+import Navbar from '../components/Navbar'
+import { AuthContext } from '../contexts/AuthContext'
+import Footer from '../components/Footer'
 
 const UserDashboard = () => {
+
+  const {user} = useContext(AuthContext);
+
   return (
-    <div>UserDashboard</div>
+    <div className='flex flex-col min-h-screen'>
+      <Navbar />
+
+      <div className='pt-24 pb-16'>
+        <div className='max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 '>
+          <h1 className="text-3xl font-bold text-gray-800 mb-6">
+            Welcome, <span className='text-indigo-500'>{(user.name).charAt(0).toUpperCase() + (user.name).slice(1).toLowerCase() }</span>
+          </h1>
+
+          <div className='grid sm:grid-cols-2 lg:grid-cols-4 gap-2 p-1 mb-6 bg-gray-100 rounded'>
+
+            <NavLink to='/dashboard/user/active-booking' className={({ isActive }) => `w-full flex justify-center py-2 border text-md rounded shadow font-medium outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 transition-all duration-300 ${ isActive ? 'border-transparent bg-indigo-500 text-white hover:bg-indigo-600' : 'text-gray-700 bg-white hover:bg-gray-50' }` }> 
+              Active Booking
+            </NavLink>
+
+            <NavLink to='/dashboard/user/booking-history' className={({ isActive }) => `w-full flex justify-center py-2 border text-md rounded shadow font-medium outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 transition-all duration-300 ${ isActive ? 'border-transparent bg-indigo-500 text-white hover:bg-indigo-600' : 'text-gray-700 bg-white hover:bg-gray-50' }` }> 
+              Booking History
+            </NavLink>
+
+            <NavLink to='/dashboard/user/notifications' className={({ isActive }) => `w-full flex justify-center py-2 border text-md rounded shadow font-medium outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 transition-all duration-300 ${ isActive ? 'border-transparent bg-indigo-500 text-white hover:bg-indigo-600' : 'text-gray-700 bg-white hover:bg-gray-50' }` }> 
+              Notifications
+            </NavLink>
+
+            <NavLink to='/dashboard/user/profile' className={({ isActive }) => `w-full flex justify-center py-2 border text-md rounded shadow font-medium outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 transition-all duration-300 ${ isActive ? 'border-transparent bg-indigo-500 text-white hover:bg-indigo-600' : 'text-gray-700 bg-white hover:bg-gray-50' }` }> 
+              Profile
+            </NavLink>
+
+          </div>
+
+          <div className='mt-10'>
+            <Outlet />                
+          </div>
+          
+
+        </div>  
+      </div>
+    
+      <Footer />
+    </div>
   )
 }
 
