dcbba9621d8336010ed9d0077c55239b8e23900f
Hasmoonn <mohamedhasmoon175@gmail.com>
2025-05-19 11:51:00 +0530

    usermodel modified

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -2,10 +2,10 @@ import mongoose from 'mongoose'
 import validator from 'validator'
 
 const userSchema = new mongoose.Schema({
-  name: {type: String, trim: true},
+  name: {type: String, trim: true, required: true},
   email: {type: String, required: true, unique: true, trim: true, lowercase: true, validate: [validator.isEmail, 'Please provide a valid email']},
   password: {type: String, required: true},
-  phone: {type: String},
+  phone: {type: String, required: true},
   role: { type: String, enum: ['user', 'owner', 'admin'], default: 'user' },
   vehicleType: {type: String},
   licensePlate: {type: String},
