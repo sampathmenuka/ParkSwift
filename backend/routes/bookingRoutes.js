708ac77917b096fae424338fda98d5b1e81bb1b0
Hasmoonn <mohamedhasmoon175@gmail.com>
2025-06-01 13:02:29 +0530

    booking route modified

--- backend/routes/bookingRoutes.js
+++ backend/routes/bookingRoutes.js
@@ -1,17 +1,16 @@
 import express from 'express'
 import {authorizeRoles, userAuth} from '../middleware/userAuth.js'
-import {cancelBooking, getActiveBookings, getBookingHistory} from '../controllers/bookingController.js'
-
+import {cancelBooking, clearBookingHistory, createBooking, getActiveBookings, getBookingHistory, markAsParked} from '../controllers/bookingController.js'
 
 
 const bookingRouter = express.Router();
 
-
 bookingRouter.get('/active-booking', userAuth, authorizeRoles('user'), getActiveBookings);
-
 bookingRouter.get('/booking-history', userAuth, authorizeRoles('user'), getBookingHistory);
-
 bookingRouter.put('/cancel/:bookingId', userAuth, authorizeRoles('user'), cancelBooking);
+bookingRouter.delete('/clear-history', userAuth, authorizeRoles('user'), clearBookingHistory);
+bookingRouter.put('/mark-parked/:id', userAuth, authorizeRoles('user'), markAsParked);
+bookingRouter.post('/create', userAuth, createBooking);
 
 
 export default bookingRouter;
